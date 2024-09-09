77d37d2b9a3412354c514ae831ba139a342c69ed
schandran1204 <sreeramchandran1204@gmail.com>
2024-09-09 17:24:22 -0400

    headless run

--- playwright.config.ts
+++ playwright.config.ts
@@ -39,7 +39,7 @@ export default defineConfig({
   projects: [
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'], headless: false },
+      use: { ...devices['Desktop Chrome'] },
     },
 
     // {
